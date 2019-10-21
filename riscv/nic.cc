e98c587d6903b139c9f146ffc39e426ce3cb6366
Alex Mallery <amallery@gmail.com>
2019-10-21 00:34:48 +0000

    Set up basic network relay for temporary nic instructions

--- riscv/nic.cc
+++ riscv/nic.cc
@@ -1,13 +1,54 @@
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <string.h>
+
 #include "nic.h"
 
-nic_t::nic_t() {
+using namespace std;
 
+int nic_t::start_client_socket(string ip_address, uint16_t port) {
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		return -1;
+	}
+	struct in_addr network_order_address;
+	int ip_conversion_retval = inet_aton(ip_address.c_str(), &network_order_address);
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr = network_order_address;
+	int connect_retval = connect(sockfd, (struct sockaddr*)&addr, sizeof(addr));
+	if (connect_retval < 0) {
+		close(sockfd);
+		return -1;
+	}
+	return sockfd;
+}
+
+nic_t::nic_t() {
+	string ip_address = "127.0.0.1";
+	uint16_t port = 9000;
+	// TODO: The backend here can also be implemented with a file
+	_nic_fd = start_client_socket(ip_address, port);
 }
 
 void nic_t::store_uint64(reg_t data) {
-	hidden_data = data;
+	if (_nic_fd < 0) {
+		return; // Do nothing if the file descriptor is invalid
+	}
+	// TODO: There is no error checking on these read and write calls
+	write(_nic_fd, &data, sizeof(data));
 }
 
 reg_t nic_t::load_uint64() {
-	return hidden_data;
+	if (_nic_fd < 0) {
+		return 0; // Load a 0 into the dest register if the
+		          // file descriptor is invalid.
+	}
+	reg_t to_return;
+	read(_nic_fd, &to_return, sizeof(to_return));
+	return to_return;
 }
--- riscv/nic.h
+++ riscv/nic.h
@@ -1,6 +1,8 @@
 #ifndef _RISCV_NIC_H
 #define _RISCV_NIC_H
 
+#include <string>
+
 #include "decode.h"
 
 class nic_t {
@@ -9,7 +11,8 @@ class nic_t {
 	reg_t load_uint64();
 	void store_uint64(reg_t data);
  private:
- 	uint64_t hidden_data = 0;
+ 	int start_client_socket(std::string ip_address, uint16_t port);
+ 	int _nic_fd = -1;
 };
 
 #endif
