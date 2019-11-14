9d75a3196cd9546a77c10e26fb90b5c34f1f8116
Alex Mallery <amallery@gmail.com>
2019-11-14 23:30:58 +0000

    Cleaned up new nic implementation, still untested

--- riscv/nic.cc
+++ riscv/nic.cc
@@ -21,42 +21,6 @@ extern const char * nic_config_data;
 
 using namespace std;
 
-void nic_t::populate_addr_maps() {
-	ifstream id_addr_file;
-	id_addr_file.open(ID_ADDR_FILENAME);
-	string line;
-	bool first_line = true;
-	while (getline(id_addr_file, line)) {
-		if (line.empty()) {
-			continue;
-		}
-		if (first_line) {
-			first_line = false;
-			continue;
-		}
-		vector<string> split_line = split(line, ",");
-		string fake_addr = split_line[0];
-		string real_addr = split_line[1];
-		uint16_t real_port = stoi(split_line[2]);
-		_real_addr_map[fake_addr] = make_pair(real_addr, real_port);
-		_fake_addr_map[make_pair(real_addr, real_port)] = fake_addr;
-	}
-	id_addr_file.close();
-}
-
-void nic_t::fake_to_real_addr(string& real_addr, uint16_t& real_port, string& fake_addr) {
-	real_addr = _real_addr_map[fake_addr].first;
-	real_port = _real_addr_map[fake_addr].second;
-}
-
-void nic_t::real_to_fake_addr(string& real_addr, uint16_t& real_port, string& fake_addr) {
-	fake_addr = _fake_addr_map[make_pair(real_addr, real_port)];
-}
-
-uint64_t nic_t::get_port_id() {
-	return 1; // TODO: Will eventually do a port id lookup based on the thread
-}
-
 nic_t::nic_t() {
 	if (nic_config_data == nullptr) {
 		return;
@@ -87,23 +51,48 @@ nic_t::~nic_t() {
 	}
 }
 
-void nic_t::store_uint64(reg_t data) {
+// ********************************************
+// Global lnic configuration interface.
+
+void nic_t::set_enable(reg_t data) {
 	if (nic_config_data == nullptr) {
+		_enable = false;
 		return;
 	}
+	if (data != 0) {
+		_enable = true;
+	} else {
+		_enable = false;
+	}
+}
+
+bool nic_t::get_enable() {
+	return _enable;
+}
+
+void nic_t::set_own_port_id(uint64_t attempted_port_id) {
 	if (!_enable) {
 		return;
 	}
-	// Technically, this allows starting to send a message before
-	// defining the per-message data. The actual write-out will fail though.
-	uint64_t port_id = get_port_id();
-	_write_message[port_id].words.push_back(data);
+	if (attempted_port_id == 0) {
+		// ID 0 is reserved, allows reading own id from same csr
+		return;
+	}
+	// Don't do anything yet, the port id can only be 1 for now
+	_write_message[1].per_message_data.src_port_id = 1;
 }
 
-reg_t nic_t::load_uint64() {
-	if (nic_config_data == nullptr) {
+uint64_t nic_t::get_port_id() {
+	if (!_enable) {
 		return 0;
 	}
+	return 1; // TODO: Will eventually do a port id lookup based on the thread
+}
+
+// ***********************************************
+// Support for the public lnic read interface.
+
+reg_t nic_t::load_uint64() {
 	if (!_enable) {
 		return 0;
 	}
@@ -142,47 +131,9 @@ reg_t nic_t::load_uint64() {
 	return next_word;
 }
 
-uint64_t nic_t::is_last_word_read() {
-	return _is_last_word[get_port_id()];
-}
-
-uint64_t nic_t::read_message_src_ip_lower() {
-	string ip_addr = _per_message_data[get_port_id()].src_ip_addr;
-	vector<string> split_addr = split(ip_addr, ".");
-	uint64_t result = 0;
-	result |= stoi(split_addr[0]) << (8*3);
-	result |= stoi(split_addr[1]) << (8*2);
-	result |= stoi(split_addr[2]) << 8;
-	result |= stoi(split_addr[3]);
-	return result;
-}
-
-uint64_t nic_t::read_message_src_ip_upper() {
-	return 0; // Not yet supported
-}
-
-uint64_t nic_t::read_message_src_port() {
-	return _per_message_data[get_port_id()].src_port_id;
-}
-
-void nic_t::set_enable(reg_t data) {
-	if (data != 0) {
-		_enable = true;
-	} else {
-		_enable = false;
-	}
-}
-
-bool nic_t::get_enable() {
-	return _enable;
-}
-
 // Mimics a read of lnic read queue ready status register
 uint64_t nic_t::num_messages_ready() {
-	if (nic_config_data == nullptr) {
-		return 0;
-	}
-	if (_enable) {
+	if (!_enable) {
 		return 0;
 	}
 	uint64_t port_id = get_port_id();
@@ -194,6 +145,76 @@ uint64_t nic_t::num_messages_ready() {
 	// being drained by one thread, so there's no risk of getting a true here and
 	// then having the queue actually be empty.
 	return num_read_messages;
+}
+
+uint64_t nic_t::is_last_word_read() {
+	if (!_enable) {
+		return 0;
+	}
+	return _is_last_word[get_port_id()];
+}
+
+uint64_t nic_t::read_src_ip_lower() {
+	if (!_enable) {
+		return 0;
+	}
+	string ip_addr = _per_message_data[get_port_id()].src_ip_addr;
+	vector<string> split_addr = split(ip_addr, ".");
+	uint64_t result = 0;
+	result |= stoi(split_addr[0]) << (8*3);
+	result |= stoi(split_addr[1]) << (8*2);
+	result |= stoi(split_addr[2]) << 8;
+	result |= stoi(split_addr[3]);
+	return result;
+}
+
+uint64_t nic_t::read_src_ip_upper() {
+	return 0; // Not yet supported
+}
+
+uint64_t nic_t::read_src_port() {
+	if (!_enable) {
+		return 0;
+	}
+	return _per_message_data[get_port_id()].src_port_id;
+}
+
+// *************************************************
+// Support for the public lnic write interface.
+
+void nic_t::store_uint64(reg_t data) {
+	if (!_enable) {
+		return;
+	}
+	// Technically, this allows starting to send a message before
+	// defining the per-message data. The actual write-out will fail though.
+	uint64_t port_id = get_port_id();
+	_write_message[port_id].words.push_back(data);
+}
+
+void nic_t::set_dst_ip_lower(uint64_t lower_ip_bits) {
+	if (!_enable) {
+		return;
+	}
+	lower_ip_bits &= 0xFFFFFFFF;
+	stringstream ss;
+	uint8_t first = (lower_ip_bits >> (8*3)) & 0xFF;
+	uint8_t second = (lower_ip_bits >> (8*2)) & 0xFF;
+	uint8_t third = (lower_ip_bits >> 8) & 0xFF;
+	uint8_t fourth = lower_ip_bits & 0xFF;
+	ss << first << "." << second << "." << third << "." << fourth;
+	_write_message[get_port_id()].per_message_data.dst_ip_addr = ss.str();
+}
+
+void nic_t::set_dst_ip_upper(uint64_t upper_ip_bits) {
+	// Only ipv4 for now
+}
+
+void nic_t::set_dst_port(uint64_t dst_port_id) {
+	if (!_enable) {
+		return;
+	}
+	_write_message[get_port_id()].per_message_data.dst_port_id = dst_port_id;
 }
 
 void nic_t::write_message_end() {
@@ -243,39 +264,10 @@ void nic_t::write_message_end() {
 	}
 }
 
-void set_own_port_id(uint64_t attempted_port_id) {
-	if (!_enable) {
-		return;
-	}
-	// Don't do anything yet, the port id can only be 1 for now
-	_write_message[1].per_message_data.src_port_id = 1;
-}
 
-void set_dst_ip_lower(uint64_t lower_ip_bits) {
-	if (!_enable) {
-		return;
-	}
-	lower_ip_bits &= 0xFFFFFFFF;
-	stringstream ss;
-	uint8_t first = (lower_ip_bits >> (8*3)) & 0xFF;
-	uint8_t second = (lower_ip_bits >> (8*2)) & 0xFF;
-	uint8_t third = (lower_ip_bits >> 8) & 0xFF;
-	uint8_t fourth = lower_ip_bits & 0xFF;
-	ss << first << "." << second << "." << third << "." << fourth;
-	_write_message[get_port_id()].per_message_data.dst_ip_addr = ss.str();
-}
 
-void set_dst_ip_upper(uint64_t upper_ip_bits) {
-	// Only ipv4 for now
-}
-
-void set_dst_port_id(uint64_t dst_port_id) {
-	if (!_enable) {
-		return;
-	}
-	_write_message[get_port_id()].per_message_data.dst_port_id = dst_port_id;
-}
-
+// ***********************************************************
+// Read processing backend and ip translation utility functions.
 
 void nic_t::handle_lnic_datagram(uint8_t* datagram, ssize_t datagram_len, string src_ip_address) {
 	// We now have the raw data and the src ip address, same as we would
@@ -389,4 +381,36 @@ vector<string> nic_t::split(string input, string delim) {
 	}
 	to_return.push_back(input);
 	return to_return;
+}
+
+void nic_t::populate_addr_maps() {
+	ifstream id_addr_file;
+	id_addr_file.open(ID_ADDR_FILENAME);
+	string line;
+	bool first_line = true;
+	while (getline(id_addr_file, line)) {
+		if (line.empty()) {
+			continue;
+		}
+		if (first_line) {
+			first_line = false;
+			continue;
+		}
+		vector<string> split_line = split(line, ",");
+		string fake_addr = split_line[0];
+		string real_addr = split_line[1];
+		uint16_t real_port = stoi(split_line[2]);
+		_real_addr_map[fake_addr] = make_pair(real_addr, real_port);
+		_fake_addr_map[make_pair(real_addr, real_port)] = fake_addr;
+	}
+	id_addr_file.close();
+}
+
+void nic_t::fake_to_real_addr(string& real_addr, uint16_t& real_port, string& fake_addr) {
+	real_addr = _real_addr_map[fake_addr].first;
+	real_port = _real_addr_map[fake_addr].second;
+}
+
+void nic_t::real_to_fake_addr(string& real_addr, uint16_t& real_port, string& fake_addr) {
+	fake_addr = _fake_addr_map[make_pair(real_addr, real_port)];
 }
--- riscv/nic.h
+++ riscv/nic.h
@@ -10,41 +10,51 @@
 
 #include "decode.h"
 
+// Internal socket management and ip translation constants.
 const int CONN_BACKLOG = 32;
-const uint64_t INVALID_ID = 0;
-const uint64_t MAP_ID = 1; // This is very wasteful reading these id's in all the time.
-// We probably want to be able to configure one message type and then have it stick around
-// for a while.
-const uint64_t REDUCE_ID = 2;
-// Host id's, not message id's
-const uint64_t ROOT_ID = 1;
-const uint64_t BRANCHING_FACTOR = 4;
 const std::string ID_ADDR_FILENAME = "/home/vagrant/firechip/lnic-dev/riscv-isa-sim/riscv/id_addr.txt";
-const uint64_t LISTEN_CONNECT_DELAY_MS = 3000;
-const uint64_t LNIC_MAGIC = 0xABCDEF12;
 const uint64_t DATAGRAM_SIZE = 1024;
+
+// Lnic header magic number.
+const uint64_t LNIC_MAGIC = 0xABCDEF12;
+
 
 class nic_t {
  public:
 	nic_t();
 	~nic_t();
-	reg_t load_uint64();
-	void store_uint64(reg_t data);
-	uint64_t num_messages_ready();
-	void write_message_end();
-	bool get_enable();
+
+	// Global configuration interface
 	void set_enable(reg_t data);
+	bool get_enable();
+	void set_own_port_id(uint64_t attempted_port_id);
+	uint64_t get_port_id();
+
+	// Read interface
+	reg_t load_uint64();
+	uint64_t num_messages_ready();
+	uint64_t is_last_word_read();
+	uint64_t read_src_ip_lower();
+	uint64_t read_src_ip_upper();
+	uint64_t read_src_port();
+
+	// Write interface
+	void store_uint64(reg_t data);
+	void set_dst_ip_lower(uint64_t lower_ip_bits);
+	void set_dst_ip_upper(uint64_t upper_ip_bits);
+	void set_dst_port(uint64_t dst_port_id);
+	void write_message_end();
  private:
+ 	// Reading and ip translation utility functions.
+ 	void handle_lnic_datagram(uint8_t* datagram, ssize_t datagram_len, std::string src_ip_address);
+ 	void listen_for_datagrams();
  	int start_server_socket(uint16_t port);
  	std::vector<std::string> split(std::string input, std::string delim);
- 	std::map<uint64_t, connection_t> get_id_addr_map();
- 	void listen_for_datagrams();
- 	void handle_lnic_datagram(uint8_t* datagram, ssize_t datagram_len, std::string src_ip_address);
+ 	void populate_addr_maps();
  	void fake_to_real_addr(std::string& real_addr, uint16_t& real_port, std::string& fake_addr);
  	void real_to_fake_addr(std::string& real_addr, uint16_t& real_port, std::string& fake_addr);
- 	uint64_t get_port_id();
 
- 	// This is the most basic assembly config interface
+ 	// Per-message metadata, used for both writing and reading.
  	struct message_data_t {
  		std::string src_ip_addr;
  		std::string dst_ip_addr;
@@ -52,37 +62,39 @@ class nic_t {
  		uint64_t dst_port_id;
  	};
 
- 	// These are the bytes that actually get written onto the wire.
- 	// For now, it's pretty much bad udp that's here to send along
- 	// out-of-band config data the lnic needs.
+ 	// Message header data actually serialized onto the wire.
  	struct header_data_t {
  		uint64_t magic;
  		uint64_t src_port_id;
  		uint64_t dst_port_id;
  	} __attribute__((packed));
 
+ 	// Used to track associated metadata for words in the read queue.
  	struct read_word_t {
  		uint64_t word;
  		bool is_last_word;
  		message_data_t* per_message_data;
  	}
 
+ 	// Used to track metadata for the message currently being written.
  	struct write_message_t {
  		std::vector<uint64_t> words;
  		message_data_t per_message_data;
  	}
 
+ 	// Read queue and support structures, for each thread
 	std::map<uint64_t, std::mutex> _read_lock;
 	std::map<uint64_t, std::queue<read_word_t>> _read_queue;
 	std::map<uint64_t, bool> _is_last_word;
 	std::map<uint64_t, message_data_t> _per_message_data;
 
+	// Write message, for each thread
 	std::map<uint64_t, write_message_t> _write_message;
 
+	// Internal socket, ip translation accounting, and global enable accounting
 	std::string _own_ip_address;
 	int _server_socket = -1;
 	int _write_socket = -1;
-
 	std::map<std::string, std::pair<std::string, uint16_t>> _real_addr_map;
 	std::map<std::pair<std::string, uint16_t>, std::string> _fake_addr_map;
  	std::vector<std::thread> _all_threads;
