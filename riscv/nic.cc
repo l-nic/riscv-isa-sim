bcb692b2b82ac6b3aa537ab9a00f32a46342ac0d
Alex Mallery <amallery@gmail.com>
2019-11-14 12:06:28 +0000

    In progress conversion of lnic spike backend to discussed format. Also switching to udp.

--- riscv/nic.cc
+++ riscv/nic.cc
@@ -21,53 +21,68 @@ extern const char * nic_config_data;
 
 using namespace std;
 
+void nic_t::populate_addr_maps() {
+	ifstream id_addr_file;
+	id_addr_file.open(ID_ADDR_FILENAME);
+	string line;
+	bool first_line = true;
+	while (getline(id_addr_file, line)) {
+		if (line.empty()) {
+			continue;
+		}
+		if (first_line) {
+			first_line = false;
+			continue;
+		}
+		vector<string> split_line = split(line, ",");
+		string fake_addr = split_line[0];
+		string real_addr = split_line[1];
+		uint16_t real_port = stoi(split_line[2]);
+		_real_addr_map[fake_addr] = make_pair(real_addr, real_port);
+		_fake_addr_map[make_pair(real_addr, real_port)] = fake_addr;
+	}
+	id_addr_file.close();
+}
+
+void nic_t::fake_to_real_addr(string& real_addr, uint16_t& real_port, string& fake_addr) {
+	real_addr = _real_addr_map[fake_addr].first;
+	real_port = _real_addr_map[fake_addr].second;
+}
+
+void nic_t::real_to_fake_addr(string& real_addr, uint16_t& real_port, string& fake_addr) {
+	fake_addr = _fake_addr_map[make_pair(real_addr, real_port)];
+}
+
+uint64_t nic_t::get_port_id() {
+	return 1; // TODO: Will eventually do a port id lookup based on the thread
+}
+
 nic_t::nic_t() {
-	cerr << "Starting lnic" << endl;
 	if (nic_config_data == nullptr) {
 		return;
 	}
-	cerr << "Non-null config data" << endl;
-	string config_data(nic_config_data);
-	vector<string> split_config_data = split(config_data, ",");
-	if (split_config_data.size() < 2) {
-		cerr << "Own ID must be defined" << endl;
-		exit(-1);
-	}
-	cerr << "Processed config data" << endl;
-	own_id = stol(split_config_data[0]);
-	for (size_t i = 1; i < split_config_data.size(); i++) {
-		server_ids.push_back(stol(split_config_data[i]));
-	}
-	cerr << "Getting id map" << endl;
-	id_addr_map = get_id_addr_map();
-	message_ids[MAP_ID] = sizeof(MapMessage) / 8;
-	message_ids[REDUCE_ID] = sizeof(ReduceMessage) / 8;
-	cerr << "Id map populated" << endl;
-
-	server_socket = start_server_socket(id_addr_map[own_id].port);
-	if (server_socket < 0) {
+	_own_ip_address(nic_config_data);
+	populate_addr_maps();
+	string real_addr;
+	uint16_t real_port;
+	fake_to_real_addr(real_addr, real_port, _own_ip_address);
+	_server_socket = start_server_socket(real_port);
+	if (_server_socket < 0) {
 		cerr << "Failed to start server socket" << endl;
 		exit(-1);
 	}
-	all_threads.push_back(move(thread([this] () {
-		listen_for_connections();
-	})));
-	cerr << "Listen thread launched" << endl;
-
-	this_thread::sleep_for(chrono::milliseconds(LISTEN_CONNECT_DELAY_MS));
-	for (uint64_t server_id : server_ids) {
-		// Connect to each listed server id
-		int sockfd = start_client_socket(id_addr_map[server_id].ip_address, id_addr_map[server_id].port);
-		if (sockfd < 0) {
-			cerr << "Error connecting to server at ip address " << id_addr_map[server_id].ip_address << " and port " << id_addr_map[server_id].port << endl;
-			continue;
-		}
-		id_addr_map[server_id].connectionfd = sockfd;
+	_write_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	if (_write_socket < 0) {
+		printf("Failed to start write socket\n");
+		exit(-1);
 	}
+	_all_threads.push_back(move(thread([this] () {
+		listen_for_datagrams();
+	})));
 }
 
 nic_t::~nic_t() {
-	for (auto& t : all_threads) {
+	for (auto& t : _all_threads) {
 		t.join();
 	}
 }
@@ -76,118 +91,250 @@ void nic_t::store_uint64(reg_t data) {
 	if (nic_config_data == nullptr) {
 		return;
 	}
-	uint64_t word = data;
-	if (write_message_index == 0) {
-		// Start of a new message, set its type
-		uint64_t message_type_id = word;
-		uint64_t message_size = message_ids[message_type_id];
-		current_write_message = reinterpret_cast<Message*>(new uint64_t[message_size]);
-		current_write_message->message_type_id = message_type_id;
-		write_message_index++;
-	} else {
-		// Other data in the message, fill it in
-		reinterpret_cast<uint64_t*>(current_write_message)[write_message_index] = word;
-		write_message_index++;
+	if (!_enable) {
+		return;
 	}
-
-	if (write_message_index == message_ids[current_write_message->message_type_id]) {
-		// Message is complete, write it out to the queue
-		write_queue.push(current_write_message); // No locks on this since it's only used in one thread in run_task
-		write_message_index = 0;
-		flush_write_queue();
-	}
+	// Technically, this allows starting to send a message before
+	// defining the per-message data. The actual write-out will fail though.
+	uint64_t port_id = get_port_id();
+	_write_message[port_id].words.push_back(data);
 }
 
 reg_t nic_t::load_uint64() {
 	if (nic_config_data == nullptr) {
 		return 0;
 	}
-	cerr << "Lnic reading data at message index " << message_index << " with enable " << enable << endl;
-	if (current_message && message_index < message_ids[current_message->message_type_id]) {
-		// We have a current message and we're not done with it yet
-		uint64_t next_word = reinterpret_cast<uint64_t*>(current_message)[message_index];
-		message_index++;
-		return next_word;
+	if (!_enable) {
+		return 0;
 	}
-
-	// We need to read a new message
-	read_lock.lock();
-	if (read_queue.empty()) {
-		cerr << "Tried to read from an empty lnic queue!" << endl;
+	// Read queue will now be a map of queues of nic_word_t's, wher
+	// each nic_word_t contains a uint64_t and a boolean to indicate whether
+	// it's the last word in the current message.
+	// The number of messages in the queue will be determined by the
+	// number of message end markers, meaning that a partially arrived
+	// message will register as 0, but a partially consumed message will register
+	// as 1.
+	uint64_t port_id = get_port_id(); // Eventually based off the thread, always 1 for now
+	_read_lock[port_id].lock();
+	if (_read_queue[port_id].empty()) {
+		printf("Tried to read from empty queue %ld\n", port_id);
 		exit(-1);
 	}
-	if (current_message) {
-		delete [] current_message; // TODO: Check that this is the right kind of delete.
-		// (It was created as an array, so probably.)
+	nic_t::read_word_t next_word = _read_queue[port_id].front();
+	_read_queue[port_id].pop();
+	if (next_word.is_last_word) {
+		_is_last_word[port_id] = true;
+		_num_read_messages[port_id]--;
+	} else {
+		_is_last_word[port_id] = false; // This means that if we keep reading
+		// past the end of one message the lrdend register will reset.
 	}
-	message_index = 0;
-	current_message = read_queue.front();
-	read_queue.pop();
-	uint64_t next_word = reinterpret_cast<uint64_t*>(current_message)[message_index];
-	message_index++;
-	read_lock.unlock();
+	_read_lock[port_id].unlock();
+
+
+	// Update the per message metadata if necessary. (I.e. the new one is non-null.)
+	if (next_word.per_message_data) {
+		if (_per_message_data[port_id] && _per_per_message_data[port_id] != next_word.per_message_data) {
+			delete _per_message_data[port_id];
+		}
+		_per_message_data[port_id] = next_word.per_message_data;
+	}
 	return next_word;
+}
+
+uint64_t nic_t::is_last_word_read() {
+	return _is_last_word[get_port_id()];
+}
+
+uint64_t nic_t::read_message_src_ip_lower() {
+	string ip_addr = _per_message_data[get_port_id()].src_ip_addr;
+	vector<string> split_addr = split(ip_addr, ".");
+	uint64_t result = 0;
+	result |= stoi(split_addr[0]) << (8*3);
+	result |= stoi(split_addr[1]) << (8*2);
+	result |= stoi(split_addr[2]) << 8;
+	result |= stoi(split_addr[3]);
+	return result;
+}
+
+uint64_t nic_t::read_message_src_ip_upper() {
+	return 0; // Not yet supported
+}
+
+uint64_t nic_t::read_message_src_port() {
+	return _per_message_data[get_port_id()].src_port_id;
 }
 
 void nic_t::set_enable(reg_t data) {
 	if (data != 0) {
-		enable = true;
+		_enable = true;
+	} else {
+		_enable = false;
 	}
 }
 
 bool nic_t::get_enable() {
-	return enable;
+	return _enable;
 }
 
 // Mimics a read of lnic read queue ready status register
-bool nic_t::lnic_ready() {
+uint64_t nic_t::num_messages_ready() {
 	if (nic_config_data == nullptr) {
-		return false;
+		return 0;
 	}
-	read_lock.lock();
-	bool ready = !read_queue.empty();
-	read_lock.unlock();
-	// Shouldn't be a problem to read this without a lock, since the queue is only
+	if (_enable) {
+		return 0;
+	}
+	uint64_t port_id = get_port_id();
+	_read_lock[port_id].lock();
+	uint64_t num_read_messages = _num_read_messages[port_id];
+	_read_lock[port_id].unlock();
+
+	// Shouldn't be a problem to return this without a lock, since the queue is only
 	// being drained by one thread, so there's no risk of getting a true here and
 	// then having the queue actually be empty.
-	return ready;
+	return num_read_messages;
 }
 
-uint64_t nic_t::get_child_id(uint64_t own_id, uint64_t i) {
-	return (BRANCHING_FACTOR * own_id) - (BRANCHING_FACTOR - 2) + i;
-}
+void nic_t::write_message_end() {
+	if (!_enable) {
+		return;
+	}
+	uint64_t port_id = get_port_id();
+	if (!_write_message[port_id].per_message_data) {
+		return; // We need the per message data or we'll have no idea where to send this message
+	}
 
-void nic_t::flush_write_queue() {
-	// This function will for now require that the first non-message-id field of every message specifies its
-	// destination id. This will be easier to set up than supporting out-of-band metadata (i.e. tcp/ip
-	// or udp setup info sent via separate control instructions).
-	while (!write_queue.empty()) {
-		Message* to_send = write_queue.front();
-		write_queue.pop();
-		cerr << "Writing message with type " << to_send->message_type_id << " and dest " << to_send->destination_id << endl;
-		int write_fd = id_addr_map[to_send->destination_id].connectionfd;
-		ssize_t written_len = write(write_fd, to_send, message_ids[to_send->message_type_id]*8);
-		if (written_len <= 0) {
-			cerr << "Error writing to destination " << to_send->destination_id << endl;
-		}
-		delete [] to_send;
+	// Build our payload for sending
+	uint64_t datagram_len = (_write_message[port_id].words.size() / sizeof(uint64_t)) + sizeof(nic_t::header_data_t);
+	uint8_t* datagram = new uint8_t[datagram_len];
+	nic_t::header_data_t* header_data = (nic_t::header_data_t*)datagram;
+	header_data->magic = LNIC_MAGIC;
+	header_data->src_port_id = _write_message[port_id].per_message_data.src_port_id;
+	header_data->dst_port_id = _write_message[port_id].per_message_data.dst_port_id;
+	uint64_t* data_start = (uint64_t*)(datagram + sizeof(nic_t::header_data_t));
+	for (size_t i = 0; i < _write_message[port_id].words.size(); i++) {
+		data_start[i] = _write_message[port_id].words[i];
+	}
+	_write_message[port_id].words.clear();
+
+	// Actually route the packet in the simulated system
+	string fake_ip_address = _write_message[port_id].per_message_data.dst_ip_addr;
+	string real_ip_addr;
+	uint16_t real_udp_port;
+	fake_to_real_addr(real_ip_addr, real_udp_port, fake_ip_address);
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(real_udp_port);
+	struct in_addr network_order_address;
+    int ip_conversion_retval = inet_aton(real_ip_addr.c_str(), &network_order_address);
+    if (ip_conversion_retval == 0) {
+        printf("Ip conversion failure\n");
+        exit(-1);
+    }
+    addr.sin_addr = network_order_address;
+
+    // Send the packet
+	ssize_t bytes_written = sendto(_write_socket, datagram, datagram_len, 0, (const struct sockaddr *)&addr, sizeof(addr));
+	delete datagram;
+	if (bytes_written <= 0) {
+		printf("Error writing packet\n");
+		exit(-1);
 	}
 }
 
-void nic_t::listen_for_connections() {
+void set_own_port_id(uint64_t attempted_port_id) {
+	if (!_enable) {
+		return;
+	}
+	// Don't do anything yet, the port id can only be 1 for now
+	_write_message[1].per_message_data.src_port_id = 1;
+}
+
+void set_dst_ip_lower(uint64_t lower_ip_bits) {
+	if (!_enable) {
+		return;
+	}
+	lower_ip_bits &= 0xFFFFFFFF;
+	stringstream ss;
+	uint8_t first = (lower_ip_bits >> (8*3)) & 0xFF;
+	uint8_t second = (lower_ip_bits >> (8*2)) & 0xFF;
+	uint8_t third = (lower_ip_bits >> 8) & 0xFF;
+	uint8_t fourth = lower_ip_bits & 0xFF;
+	ss << first << "." << second << "." << third << "." << fourth;
+	_write_message[get_port_id()].per_message_data.dst_ip_addr = ss.str();
+}
+
+void set_dst_ip_upper(uint64_t upper_ip_bits) {
+	// Only ipv4 for now
+}
+
+void set_dst_port_id(uint64_t dst_port_id) {
+	if (!_enable) {
+		return;
+	}
+	_write_message[get_port_id()].per_message_data.dst_port_id = dst_port_id;
+}
+
+
+void nic_t::handle_lnic_datagram(uint8_t* datagram, ssize_t datagram_len, string src_ip_address) {
+	// We now have the raw data and the src ip address, same as we would
+	// after deciphering the outer layers of a packet in a hardware implementation.
+	// This implementation assumes that each packet contains either one full message
+	// or part of one full message. Given that we're trying to minimize latency,
+	// packing multiple messages into one packet (thus delaying the earlier ones)
+	// would probably be a bad idea.
+
+	// Everything in this section deals with lnic processing. This is fixed.
+	if (datagram_len < sizeof(nic_t::header_data_t)) {
+		return; // Drop packets that are too short
+	}
+	nic_t::header_data_t* lnic_header = (nic_t::header_data_t*)datagram;
+	uint32_t lnic_magic = lnic_header->magic;
+	if (lnic_magic != LNIC_MAGIC) {
+		return; // Not an lnic packet, should never have been sent here
+	}
+	if (datagram_len - sizeof(nic_t::header_data_t) % sizeof(uint64_t) != 0) {
+		// All lnic packets should have word-aligned data
+		return;
+	}
+	uint64_t* data_ptr = (uint64_t*)(datagram + sizeof(nic_t::header_data_t));
+	uint64_t word_len = (datagram_len - sizeof(nic_t::header_data_t)) / sizeof(uint64_t);
+	uint64_t port_id = lnic_header->dst_port_id;
+	nic_t::header_data_t* per_message_data = new nic_t::per_message_data;
+	per_message_data->dst_port_id = lnic_header->dst_port_id;
+	per_message_data->src_port_id = lnic_header->src_port_id;
+	per_message_data->src_ip_addr = src_ip_addr;
+	per_message_data->dst_ip_addr = _own_ip_address;
+
+	// Everything in this section deals with pushing the received data into the
+	// read queue for its destination thread. This section can be changed to
+	// mimic implementing a transport protocol in hardware. Anything that gets
+	// queued up still needs an end-marker and a 4-tuple, but there's no
+	// requirement that that happen right away.
+	_read_lock[port_id].lock();
+	for (uint64_t i = 0; i < word_len; i++) {
+		bool is_end = (i == word_len - 1);
+		_read_queue[port_id].push({data_ptr[i], is_end, per_message_data});
+	}
+	_num_read_messages[port_id]++;
+	_read_lock[port_id].unlock();
+}
+
+void nic_t::listen_for_datagrams() {
+	uint8_t datagram[DATAGRAM_SIZE];
 	while (true) {
 		struct sockaddr_in addr;
 		socklen_t addr_len = sizeof(addr);
-		cout << "Waiting for connection..." << endl;
-		int connectionfd = accept(server_socket, (struct sockaddr *)
-				&addr, &addr_len);
-		if (connectionfd < 0) {
-			cerr << "Server connection accept attempt failed."
+		ssize_t bytes_received = recvfrom(_server_socket, datagram, DATAGRAM_SIZE, MSG_WAITALL, (struct sockaddr *)&addr, &addr_len);
+
+		if (bytes_received < 0) {
+			cerr << "Datagram receive attempt failed."
 				<< endl;
 			break;
 		}
 		if (addr.sin_family != AF_INET) {
-			cerr << "Server accepted non-internet connection."
+			cerr << "Server accepted non-internet datagram."
 				<< endl;
 			break;
 		}
@@ -197,17 +344,14 @@ void nic_t::listen_for_connections() {
 		inet_ntop(AF_INET, &addr.sin_addr.s_addr, dst_cstring,
 				INET_ADDRSTRLEN + 1);
 		string ip_address(dst_cstring);
-		nic_t::connection_t conn;
-		conn.connectionfd = connectionfd;
-		conn.port = port;
-		conn.ip_address = ip_address;
-		all_threads.push_back(move(thread([this, conn] () {
-						handle_connection(conn); })));
+		string fake_ip_address;
+		real_to_fake_addr(ip_address, port, fake_ip_address);
+		handle_lnic_datagram(datagram, bytes_received, fake_ip_address);
 	}
 }
 
 int nic_t::start_server_socket(uint16_t port) {
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sockfd < 0) {
 		cerr << "Server socket creation failed." << endl;
 		return -1;
@@ -230,40 +374,7 @@ int nic_t::start_server_socket(uint16_t port) {
 		return -1;
 	}
 
-	int listen_retval = listen(sockfd, CONN_BACKLOG);
-	if (listen_retval < 0) {
-		cerr << "Server socket listen failure." << endl;
-		return -1;
-	}
 	return sockfd;
-}
-
-void nic_t::handle_connection(nic_t::connection_t conn) {
-	int message_index = 0;
-	while (true) {
-		// This scheme requires sending the message type id first
-		uint64_t message_type_id;
-		cout << "Connection waiting for data..." << endl;
-		ssize_t actual_len = read(conn.connectionfd, &message_type_id, 8);
-		if (actual_len <= 0) {
-			cerr << "Read failure at server in header" << endl;
-			break;
-		}
-		cerr << "Message type id is " << message_type_id << endl;
-		uint64_t message_size = message_ids[message_type_id];
-		uint64_t* buffer = new uint64_t[message_size];
-		buffer[0] = message_type_id;
-		cerr << "Message size is " << message_size << endl;
-		actual_len = read(conn.connectionfd, buffer + 1, (message_size-1)*8);
-		if (actual_len <= 0) {
-			cerr << "Read failure at server." << endl;
-			break;
-		}
-		read_lock.lock();
-		read_queue.push(reinterpret_cast<Message*>(buffer));
-		read_lock.unlock();
-	
-	}
 }
 
 vector<string> nic_t::split(string input, string delim) {
@@ -279,47 +390,3 @@ vector<string> nic_t::split(string input, string delim) {
 	to_return.push_back(input);
 	return to_return;
 }
-
-map<uint64_t, nic_t::connection_t> nic_t::get_id_addr_map() {
-	ifstream id_addr_file;
-	id_addr_file.open(ID_ADDR_FILENAME);
-	string line;
-	bool first_line = true;
-	map<uint64_t, nic_t::connection_t> id_addr_map;
-	while (getline(id_addr_file, line)) {
-		if (line.empty()) {
-			continue;
-		}
-		if (first_line) {
-			first_line = false;
-			continue;
-		}
-		vector<string> split_line = split(line, ",");
-		uint64_t id = stol(split_line[0]);
-		string ip_addr = split_line[1];
-		uint16_t port = stoi(split_line[2]);
-		id_addr_map[id] = {-1, port, ip_addr};
-	}
-	id_addr_file.close();
-	return id_addr_map;
-}
-
-int nic_t::start_client_socket(string ip_address, uint16_t port) {
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) {
-		return -1;
-	}
-	struct in_addr network_order_address;
-	int ip_conversion_retval = inet_aton(ip_address.c_str(), &network_order_address);
-	struct sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(port);
-	addr.sin_addr = network_order_address;
-	int connect_retval = connect(sockfd, (struct sockaddr*)&addr, sizeof(addr));
-	if (connect_retval < 0) {
-		close(sockfd);
-		return -1;
-	}
-	return sockfd;
-}
-
--- riscv/nic.h
+++ riscv/nic.h
@@ -21,6 +21,8 @@ const uint64_t ROOT_ID = 1;
 const uint64_t BRANCHING_FACTOR = 4;
 const std::string ID_ADDR_FILENAME = "/home/vagrant/firechip/lnic-dev/riscv-isa-sim/riscv/id_addr.txt";
 const uint64_t LISTEN_CONNECT_DELAY_MS = 3000;
+const uint64_t LNIC_MAGIC = 0xABCDEF12;
+const uint64_t DATAGRAM_SIZE = 1024;
 
 class nic_t {
  public:
@@ -28,57 +30,63 @@ class nic_t {
 	~nic_t();
 	reg_t load_uint64();
 	void store_uint64(reg_t data);
-	bool lnic_ready();
+	uint64_t num_messages_ready();
+	void write_message_end();
 	bool get_enable();
 	void set_enable(reg_t data);
  private:
-	struct connection_t {
-		int connectionfd;
-		uint16_t port;
-		std::string ip_address;
-	};
-	// Might want to see how feasible it is to use protobufs here.
-	struct Message {
-		uint64_t message_type_id = INVALID_ID;
-		uint64_t destination_id = INVALID_ID;
-	};
-	struct MapMessage : Message {
-		uint64_t max_depth = 3;
-		uint64_t cur_depth = 0;
-		uint64_t src_host_id = INVALID_ID;
-	};
-	struct ReduceMessage : Message {
-
-	};
-	struct MessageState {
-		uint64_t response_cnt = 0;
-		uint64_t map_cnt = BRANCHING_FACTOR;
-		uint64_t src_host_id = INVALID_ID;
-	};
-
-	int start_client_socket(std::string ip_address, uint16_t port);
  	int start_server_socket(uint16_t port);
- 	void handle_connection(connection_t conn);
  	std::vector<std::string> split(std::string input, std::string delim);
  	std::map<uint64_t, connection_t> get_id_addr_map();
- 	void flush_write_queue();
- 	uint64_t get_child_id(uint64_t own_id, uint64_t i);
- 	void listen_for_connections();
+ 	void listen_for_datagrams();
+ 	void handle_lnic_datagram(uint8_t* datagram, ssize_t datagram_len, std::string src_ip_address);
+ 	void fake_to_real_addr(std::string& real_addr, uint16_t& real_port, std::string& fake_addr);
+ 	void real_to_fake_addr(std::string& real_addr, uint16_t& real_port, std::string& fake_addr);
+ 	uint64_t get_port_id();
 
- 	std::vector<std::thread> all_threads;
-	std::mutex read_lock;
-	std::queue<Message*> read_queue;
-	std::queue<Message*> write_queue;
-	std::map<uint64_t, uint64_t> message_ids;
-	Message* current_message = nullptr;
-	uint64_t message_index = 0;
-	Message* current_write_message = nullptr;
-	uint64_t write_message_index = 0;
-	std::vector<uint64_t> server_ids;
-	std::map<uint64_t, connection_t> id_addr_map;
-	int server_socket = -1;
-	uint64_t own_id = INVALID_ID;
-	bool enable = false;
+ 	// This is the most basic assembly config interface
+ 	struct message_data_t {
+ 		std::string src_ip_addr;
+ 		std::string dst_ip_addr;
+ 		uint64_t src_port_id;
+ 		uint64_t dst_port_id;
+ 	};
+
+ 	// These are the bytes that actually get written onto the wire.
+ 	// For now, it's pretty much bad udp that's here to send along
+ 	// out-of-band config data the lnic needs.
+ 	struct header_data_t {
+ 		uint64_t magic;
+ 		uint64_t src_port_id;
+ 		uint64_t dst_port_id;
+ 	} __attribute__((packed));
+
+ 	struct read_word_t {
+ 		uint64_t word;
+ 		bool is_last_word;
+ 		message_data_t* per_message_data;
+ 	}
+
+ 	struct write_message_t {
+ 		std::vector<uint64_t> words;
+ 		message_data_t per_message_data;
+ 	}
+
+	std::map<uint64_t, std::mutex> _read_lock;
+	std::map<uint64_t, std::queue<read_word_t>> _read_queue;
+	std::map<uint64_t, bool> _is_last_word;
+	std::map<uint64_t, message_data_t> _per_message_data;
+
+	std::map<uint64_t, write_message_t> _write_message;
+
+	std::string _own_ip_address;
+	int _server_socket = -1;
+	int _write_socket = -1;
+
+	std::map<std::string, std::pair<std::string, uint16_t>> _real_addr_map;
+	std::map<std::pair<std::string, uint16_t>, std::string> _fake_addr_map;
+ 	std::vector<std::thread> _all_threads;
+	bool _enable = false;
 };
 
 #endif
